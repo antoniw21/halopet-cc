d7167541c2b5d72d75e4dd02ba3c1303dc905d08
Antoni Wijaya <ton302107@gmail.com>
2023-06-08 20:26:46 +0700

    enable firestore

--- src/handler.js
+++ src/handler.js
@@ -73,9 +73,9 @@ const registerNewUserHandler = async (request, h) => {
     }
 
     const addUserToFirestore = await db.collection('users').doc(`${userRecord.uid}`).set(newUser);
-    // const addPetToFirestore = await db.collection('pets').doc(`${userRecord.uid}`).set(newUserPet);
-    // const fotToFirestore = await db.collection('users').doc(`${userRecord.uid}`).collection('foto').doc('1').set(colFoto);
-    // console.log('user added to firestore');
+    const addPetToFirestore = await db.collection('pets').doc(`${userRecord.uid}`).set(newUserPet);
+    const fotToFirestore = await db.collection('users').doc(`${userRecord.uid}`).collection('foto').doc('1').set(colFoto);
+    console.log('user added to firestore');
 
     const response = h.response({
       status: 'success',
